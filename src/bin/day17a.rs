8346d5561bc36020828c050be2c716699e3fe8c8
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-12-17 22:18:18 +0000

    Day 17, using itertools

---
+++ src/bin/day17a.rs
@@ -0,0 +1,105 @@
+// Copyright (C) 2020 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use anyhow::Result;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::io::{stdin, BufRead};
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct XYZ {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl XYZ {
+    const COORDS: [XYZ; 26] = [
+        XYZ {
+            x: -1,
+            y: -1,
+            z: -1,
+        },
+        XYZ { x: -1, y: -1, z: 0 },
+        XYZ { x: -1, y: -1, z: 1 },
+        XYZ { x: -1, y: 0, z: -1 },
+        XYZ { x: -1, y: 0, z: 0 },
+        XYZ { x: -1, y: 0, z: 1 },
+        XYZ { x: -1, y: 1, z: -1 },
+        XYZ { x: -1, y: 1, z: 0 },
+        XYZ { x: -1, y: 1, z: 1 },
+        XYZ { x: 0, y: -1, z: -1 },
+        XYZ { x: 0, y: -1, z: 0 },
+        XYZ { x: 0, y: -1, z: 1 },
+        XYZ { x: 0, y: 0, z: -1 },
+        // XYZ{ x:0, y:0,z: 0},
+        XYZ { x: 0, y: 0, z: 1 },
+        XYZ { x: 0, y: 1, z: -1 },
+        XYZ { x: 0, y: 1, z: 0 },
+        XYZ { x: 0, y: 1, z: 1 },
+        XYZ { x: 1, y: -1, z: -1 },
+        XYZ { x: 1, y: -1, z: 0 },
+        XYZ { x: 1, y: -1, z: 1 },
+        XYZ { x: 1, y: 0, z: -1 },
+        XYZ { x: 1, y: 0, z: 0 },
+        XYZ { x: 1, y: 0, z: 1 },
+        XYZ { x: 1, y: 1, z: -1 },
+        XYZ { x: 1, y: 1, z: 0 },
+        XYZ { x: 1, y: 1, z: 1 },
+    ];
+
+    pub fn new(x: i32, y: i32, z: i32) -> XYZ {
+        XYZ { x, y, z }
+    }
+
+    pub fn neighs<'a>(&'a self) -> impl Iterator<Item = XYZ> + 'a {
+        XYZ::COORDS
+            .iter()
+            .map(move |xyz| XYZ::new(xyz.x + self.x, xyz.y + self.y, xyz.z + self.z))
+    }
+}
+
+// Process, etc //
+
+fn process(bufin: impl BufRead) -> Result<usize> {
+    let mut cubes = BTreeSet::default();
+    for (y, line_opt) in bufin.lines().enumerate() {
+        let line = line_opt?;
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                cubes.insert(XYZ::new(x as i32, y as i32, 0 as i32));
+            }
+        }
+    }
+    for _ in 0..6 {
+        let mut neighs = BTreeMap::default();
+        for cube in &cubes {
+            for neigh in cube.neighs() {
+                let e = neighs.entry(neigh).or_insert(0);
+                *e += 1;
+            }
+        }
+        let mut newcubes = BTreeSet::default();
+        for (xyz, n) in neighs {
+            if n == 3 || n == 2 && cubes.contains(&xyz) {
+                newcubes.insert(xyz);
+            }
+        }
+        cubes = newcubes;
+    }
+    Ok(cubes.len())
+}
+
+#[test]
+fn test() -> Result<()> {
+    let input: &[u8] = b".#.\n..#\n###\n";
+    eprintln!("");
+    assert_eq!(process(input)?, 112);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("{}", process(stdin().lock())?);
+    Ok(())
+}
---
+++ src/bin/day17b.rs
@@ -0,0 +1,93 @@
+// Copyright (C) 2020 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use anyhow::Result;
+use itertools::iproduct;
+use lazy_static::lazy_static;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::io::{stdin, BufRead};
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct XYZW {
+    x: i32,
+    y: i32,
+    z: i32,
+    w: i32,
+}
+
+lazy_static! {
+    static ref COORDS: Vec<XYZW> = {
+        iproduct!(0..3, 0..3, 0..3, 0..3)
+            .filter_map(|(a, b, c, d)| {
+                if a == 1 && b == 1 && c == 1 && d == 1 {
+                    None
+                } else {
+                    Some(XYZW::new(a - 1, b - 1, c - 1, d - 1))
+                }
+            })
+            .collect()
+    };
+}
+
+impl XYZW {
+    pub fn new(x: i32, y: i32, z: i32, w: i32) -> XYZW {
+        XYZW { x, y, z, w }
+    }
+
+    pub fn neighs<'a>(&'a self) -> impl Iterator<Item = XYZW> + 'a {
+        COORDS.iter().map(move |xyzw| {
+            XYZW::new(
+                xyzw.x + self.x,
+                xyzw.y + self.y,
+                xyzw.z + self.z,
+                xyzw.w + self.w,
+            )
+        })
+    }
+}
+
+// Process, etc //
+
+fn process(bufin: impl BufRead) -> Result<usize> {
+    let mut cubes = BTreeSet::default();
+    for (y, line_opt) in bufin.lines().enumerate() {
+        let line = line_opt?;
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                cubes.insert(XYZW::new(x as i32, y as i32, 0 as i32, 0 as i32));
+            }
+        }
+    }
+    for _ in 0..6 {
+        let mut neighs = BTreeMap::default();
+        for cube in &cubes {
+            for neigh in cube.neighs() {
+                let e = neighs.entry(neigh).or_insert(0);
+                *e += 1;
+            }
+        }
+        let mut newcubes = BTreeSet::default();
+        for (xyz, n) in neighs {
+            if n == 3 || n == 2 && cubes.contains(&xyz) {
+                newcubes.insert(xyz);
+            }
+        }
+        cubes = newcubes;
+    }
+    Ok(cubes.len())
+}
+
+#[test]
+fn test() -> Result<()> {
+    let input: &[u8] = b".#.\n..#\n###\n";
+    eprintln!("");
+    assert_eq!(process(input)?, 848);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("{}", process(stdin().lock())?);
+    Ok(())
+}
